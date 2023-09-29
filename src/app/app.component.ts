e3ef4147949942fb86701d795e9dc72e03a5f3ea
Eduardo Andrade Orthmann <orthmann2811@gmail.com>
2023-09-28 21:50:37 -0300

    add input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-test';
+  name = 'Eduardo';
+  firstGrade = 0;
+  secondGrade = 0;
+  thirdGrade = 0;
+  average: number | null = null;
+
+  handleButtonClick() {
+    console.log('clicked');
+  }
+
+  handleCalculateAverageClick() {
+    this.average = (this.firstGrade + this.secondGrade + this.thirdGrade) / 3;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
+import { FormsModule } from '@angular/forms';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    InputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
