cbf403c2b0997e1ff8a1f3139fd46f1a4198c69b
Eduardo <orthmann2811@gmail.com>
2023-09-30 00:57:08 -0300

    add components directory

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { FirstExercisesListComponent } from './first-exercises-list/first-exercises-list.component';
+import { HomeComponent } from './components/home/home.component';
+import { FirstExercisesListComponent } from './components/first-exercises-list/first-exercises-list.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'first-exercises-list', component: FirstExercisesListComponent }
-
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './button/button.component';
+import { ButtonComponent } from './components/button/button.component';
 import { FormsModule } from '@angular/forms';
-import { CardComponent } from './card/card.component';
-import { InputComponent } from './input/input.component';
-import { HeaderComponent } from './header/header.component';
+import { CardComponent } from './components/card/card.component';
+import { InputComponent } from './components/input/input.component';
+import { HeaderComponent } from './components/header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './home/home.component';
-import { FirstExercisesListComponent } from './first-exercises-list/first-exercises-list.component';
+import { HomeComponent } from './components/home/home.component';
+import { FirstExercisesListComponent } from './components/first-exercises-list/first-exercises-list.component';
 
 @NgModule({
   declarations: [
